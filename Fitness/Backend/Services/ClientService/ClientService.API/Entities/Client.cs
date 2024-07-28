63040f520763e259bb2519ec15527aa62d6e2216
Vasilije <vasatodorovic1@gmail.com>
2024-07-28 23:21:28 +0200

    Dodat ClientService

---
+++ Fitness/Backend/Services/ClientService/ClientService.API/Entities/Client.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace ClientService.API.Entities
+{
+    public class Client
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string TrainerId { get; set; }
+        public int CurrentProgramId { get; set; }
+        public string ImageFile { get; set; }
+
+    }
+}
---
+++ Fitness/Backend/Services/ClientService/ClientService.API/Repositories/Repository.cs
@@ -0,0 +1,52 @@
+﻿using ClientService.API.Data;
+using ClientService.API.Entities;
+using MongoDB.Driver;
+
+namespace ClientService.API.Repositories
+{
+    public class Repository: IRepository
+    {
+        private readonly IClientContext _clientContext;
+
+        public Repository(IClientContext clientContext)
+        {
+            _clientContext = clientContext ?? throw new ArgumentNullException(nameof(clientContext));
+        }
+        public async Task<IEnumerable<Client>> GetClients()
+        {
+            return await _clientContext.Clients.Find(p => true).ToListAsync();
+        }
+        public async Task<Client> GetClientById(string id)
+        {
+            return await _clientContext.Clients.Find(p => p.Id == id).FirstOrDefaultAsync();
+        }
+        public async Task<IEnumerable<Client>> GetClientsByName(string name)
+        {
+            return await _clientContext.Clients.Find(p => p.Name == name).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Client>> GetClientsBySurname(string surname)
+        {
+            return await _clientContext.Clients.Find(p => p.Surname == surname).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Client>> GetClientsByTrainerId(string trainerId)
+        {
+            return await _clientContext.Clients.Find(p => p.TrainerId == trainerId).ToListAsync();
+        }
+        public async Task CreateClient(Client client)
+        {
+            await _clientContext.Clients.InsertOneAsync(client);
+        }
+        public async Task<bool> DeleteClient(string id)
+        {
+            var result = await _clientContext.Clients.DeleteOneAsync(p => p.Id == id);
+            return result.IsAcknowledged && result.DeletedCount > 0;
+        }
+        public async Task<bool> UpdateClient(Client client)
+        {
+            var result = await _clientContext.Clients.ReplaceOneAsync(p => p.Id == client.Id, client);
+            return result.IsAcknowledged && result.ModifiedCount > 0;
+        }
+    }
+}
